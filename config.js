7326f4a606f9735a5fd0c8381bdabca241d4fcc6
dehio <hkawaras@gmail.com>
2019-08-18 23:19:34 +0900

    Add google Analytics

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '© 2019 dehio_ All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: '',
+  googleAnalyticsId: 'UA-145860915-1',
   useKatex: false,
   menu: [
     {
